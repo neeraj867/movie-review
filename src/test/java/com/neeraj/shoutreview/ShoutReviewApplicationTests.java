02f160e285a5f8eced62dbc98914a62304e42410
Neeraj <neeraj19082001@gmail.com>
2023-10-20 16:24:21 +0530

    project initialize

---
+++ src/test/java/com/neeraj/shoutreview/ShoutReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.shoutreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoutReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
